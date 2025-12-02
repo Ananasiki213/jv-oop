b6492de4f7c2e9774308665066a7009b170477db
Super <meizum3notewh@gmail.com>
2025-12-02 21:12:04 +0200

    Done task

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+
+    @Override
+
+    public void doWork() {
+        System.out.println("Truck Is Working");
+    }
+
+    @Override
+
+    public void stopWork() {
+        System.out.println("Truck Stopped Working");
+    }
+}
+
+
+
+
+
+
