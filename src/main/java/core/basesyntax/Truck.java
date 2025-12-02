0fcea743d86ed79eb4c3669a0aac672e6e0af9e6
Super <meizum3notewh@gmail.com>
2025-12-02 21:31:39 +0200

    Fixed

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
+
     @Override
     public void doWork() {
         System.out.println("Truck Is Working");
     }
+
     @Override
     public void stopWork() {
         System.out.println("Truck Stopped Working");
